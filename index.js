4cbff999904101c5fe931ba716c9d38190290067
Soren <sorenisme@gmail.com>
2022-09-21 10:09:47 -0400

    commit tree 50f35fb61eac319699bd122aa69262d0d2864412

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+const LeastFavoriteCustomer = 'joe'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = 'jane'
+}
